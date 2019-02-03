a950df11022d2b68edd951211b3e42f9aa0d1dbc
3ergEapTeam <47082744+3ergEapTeam@users.noreply.github.com>
2019-02-03 15:31:52 +0200

    Add files via upload

---
+++ MovieDatabase/src/moviedatabase/FavoriteList.java
@@ -0,0 +1,99 @@
+package moviedatabase;
+
+import java.io.Serializable;
+import java.util.Collection;
+import javax.persistence.Basic;
+import javax.persistence.CascadeType;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.NamedQueries;
+import javax.persistence.NamedQuery;
+import javax.persistence.OneToMany;
+import javax.persistence.Table;
+import javax.xml.bind.annotation.XmlRootElement;
+import javax.xml.bind.annotation.XmlTransient;
+
+@Entity
+@Table(name = "FAVORITE_LIST")
+@XmlRootElement
+@NamedQueries({
+    @NamedQuery(name = "FavoriteList.findAll", query = "SELECT f FROM FavoriteList f")
+    , @NamedQuery(name = "FavoriteList.findById", query = "SELECT f FROM FavoriteList f WHERE f.id = :id")
+    , @NamedQuery(name = "FavoriteList.findByName", query = "SELECT f FROM FavoriteList f WHERE f.name = :name")})
+public class FavoriteList implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+    @Id
+    @Basic(optional = false)
+    @Column(name = "ID")
+    private Integer id;
+    @Basic(optional = false)
+    @Column(name = "NAME")
+    private String name;
+    @OneToMany(cascade = CascadeType.ALL, mappedBy = "favoriteListId")
+    private Collection<Movie> movieCollection;
+
+    public FavoriteList() {
+    }
+
+    public FavoriteList(Integer id) {
+        this.id = id;
+    }
+
+    public FavoriteList(Integer id, String name) {
+        this.id = id;
+        this.name = name;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    @XmlTransient
+    public Collection<Movie> getMovieCollection() {
+        return movieCollection;
+    }
+
+    public void setMovieCollection(Collection<Movie> movieCollection) {
+        this.movieCollection = movieCollection;
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 0;
+        hash += (id != null ? id.hashCode() : 0);
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object object) {
+        // TODO: Warning - this method won't work in the case the id fields are not set
+        if (!(object instanceof FavoriteList)) {
+            return false;
+        }
+        FavoriteList other = (FavoriteList) object;
+        if ((this.id == null && other.id != null) || (this.id != null && !this.id.equals(other.id))) {
+            return false;
+        }
+        return true;
+    }
+
+    @Override
+    public String toString() {
+        return "moviedatabase.FavoriteList[ id=" + id + " ]";
+    }
+    
+}
---
+++ MovieDatabase/src/moviedatabase/Movie.java
@@ -0,0 +1,151 @@
+package moviedatabase;
+
+import java.io.Serializable;
+import java.util.Date;
+import javax.persistence.Basic;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.NamedQueries;
+import javax.persistence.NamedQuery;
+import javax.persistence.Table;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
+import javax.xml.bind.annotation.XmlRootElement;
+
+@Entity
+@Table(name = "MOVIE")
+@XmlRootElement
+@NamedQueries({
+    @NamedQuery(name = "Movie.findAll", query = "SELECT m FROM Movie m")
+    , @NamedQuery(name = "Movie.findById", query = "SELECT m FROM Movie m WHERE m.id = :id")
+    , @NamedQuery(name = "Movie.findByTitle", query = "SELECT m FROM Movie m WHERE m.title = :title")
+    , @NamedQuery(name = "Movie.findByReleaseDate", query = "SELECT m FROM Movie m WHERE m.releaseDate = :releaseDate")
+    , @NamedQuery(name = "Movie.findByRating", query = "SELECT m FROM Movie m WHERE m.rating = :rating")
+    , @NamedQuery(name = "Movie.findByOverview", query = "SELECT m FROM Movie m WHERE m.overview = :overview")})
+public class Movie implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+    @Id
+    @Basic(optional = false)
+    @Column(name = "ID")
+    private Integer id;
+    @Basic(optional = false)
+    @Column(name = "TITLE")
+    private String title;
+    @Basic(optional = false)
+    @Column(name = "RELEASE_DATE")
+    @Temporal(TemporalType.DATE)
+    private Date releaseDate;
+    @Basic(optional = false)
+    @Column(name = "RATING")
+    private double rating;
+    @Basic(optional = false)
+    @Column(name = "OVERVIEW")
+    private String overview;
+    @JoinColumn(name = "FAVORITE_LIST_ID", referencedColumnName = "ID")
+    @ManyToOne(optional = false)
+    private FavoriteList favoriteListId;
+    @JoinColumn(name = "GENRE_ID", referencedColumnName = "ID")
+    @ManyToOne(optional = false)
+    private Genre genreId;
+
+    public Movie() {
+    }
+
+    public Movie(Integer id) {
+        this.id = id;
+    }
+
+    public Movie(Integer id, String title, Date releaseDate, double rating, String overview) {
+        this.id = id;
+        this.title = title;
+        this.releaseDate = releaseDate;
+        this.rating = rating;
+        this.overview = overview;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public Date getReleaseDate() {
+        return releaseDate;
+    }
+
+    public void setReleaseDate(Date releaseDate) {
+        this.releaseDate = releaseDate;
+    }
+
+    public double getRating() {
+        return rating;
+    }
+
+    public void setRating(double rating) {
+        this.rating = rating;
+    }
+
+    public String getOverview() {
+        return overview;
+    }
+
+    public void setOverview(String overview) {
+        this.overview = overview;
+    }
+
+    public FavoriteList getFavoriteListId() {
+        return favoriteListId;
+    }
+
+    public void setFavoriteListId(FavoriteList favoriteListId) {
+        this.favoriteListId = favoriteListId;
+    }
+
+    public Genre getGenreId() {
+        return genreId;
+    }
+
+    public void setGenreId(Genre genreId) {
+        this.genreId = genreId;
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 0;
+        hash += (id != null ? id.hashCode() : 0);
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object object) {
+        // TODO: Warning - this method won't work in the case the id fields are not set
+        if (!(object instanceof Movie)) {
+            return false;
+        }
+        Movie other = (Movie) object;
+        if ((this.id == null && other.id != null) || (this.id != null && !this.id.equals(other.id))) {
+            return false;
+        }
+        return true;
+    }
+
+    @Override
+    public String toString() {
+        return "moviedatabase.Movie[ id=" + id + " ]";
+    }
+    
+}
